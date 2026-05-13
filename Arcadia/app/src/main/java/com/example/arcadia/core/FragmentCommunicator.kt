793fc2af4e0bd8d418d46a38001b6592c9e9f711
Marvin Betancourt <marvinbetancourt41@gmail.com>
2026-05-13 15:10:17 -0600

    Se modifico todo

---
+++ Arcadia/app/src/main/java/com/example/arcadia/core/FragmentCommunicator.kt
@@ -0,0 +1,5 @@
+package com.example.arcadia.core
+
+interface FragmentCommunicator {
+    fun manageLoader(isVisible: Boolean)
+}
---
+++ Arcadia/app/src/main/java/com/example/arcadia/home/wishlist/WishlistFragment.kt
@@ -0,0 +1,14 @@
+package com.example.arcadia.home.wishlist
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.arcadia.R
+
+class WishlistFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_wishlist, container, false)
+    }
+}
